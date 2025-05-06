9e842f424a7ab968acf9f1ff1f085fd3f67798a5
Matt Clarke-Lauer <mcl@meta.com>
2025-05-06 14:57:38 -0400

    fixed error when out_of_repo_commands directory doesn

--- src/cli.py
+++ src/cli.py
@@ -15,7 +15,6 @@ from util.config import config_file
 import ccl
 import pathlib
 
-
 @click.group()
 @click.option(
     "-c",
@@ -40,6 +39,6 @@ cli.add_command(gemini)
 cli.add_command(config)
 cli.add_command(scrape)
 
-
-path_to_commands = pathlib.Path(__file__, "..", "out_of_repo_commands")
-ccl.register_commands(cli, path_to_commands)
+if pathlib.Path('out_of_repo_commands').is_dir():
+    path_to_commands = pathlib.Path(__file__, "..", "out_of_repo_commands")
+    ccl.register_commands(cli, path_to_commands)
